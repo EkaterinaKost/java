a327961b84995277cbdb138ec62a8878221cd9c1
ekaterina <kostcat-84@mail.ru>
2024-06-12 15:22:35 +0500

    hw2

---
+++ react/hw2/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import CommentsList from './components/CommentsList';
+import "./components/CommentsStyle.css";
+function App() {
+  return (
+    <div >
+      <CommentsList/>
+    </div>
+  );
+}
+
+export default App;
