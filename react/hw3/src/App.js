d8a2ca6401c70641aeb2534a412c6d1ce06bf242
ekaterina <kostcat-84@mail.ru>
2024-06-15 23:04:24 +0500

    hw3

---
+++ react/hw3/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import React from 'react';
+import TemperatureConverter from './components/TemperatureConverter';
+
+
+
+function App() {
+  return (
+    <div>
+      <TemperatureConverter/>
+    </div>
+  );
+}
+
+export default App;
